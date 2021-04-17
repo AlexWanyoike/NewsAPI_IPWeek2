6519c4c02327acfdfec7c2b5475f96f2c1ee067d
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-17 10:02:13 +0300

    Processing Results

--- app/request.py
+++ app/request.py
@@ -1,4 +1,55 @@
 from app import app
+import urllib.request,json
+from .models import new
+
+New = new.New
 
 # Getting api key
 api_key = app.config['NEW_API_KEY']
+
+# Getting the movie base url
+base_url = app.config["NEW_API_BASE_URL"]
+
+def get_news(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        new_results = None
+
+        if get_news_response['results']:
+            new_results_list = get_news_response['results']
+            new_results = process_results(new_results_list)
+
+
+    return new_results
+
+def process_results(new_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        movie_list: A list of dictionaries that contain movie details
+
+    Returns :
+        movie_results: A list of movie objects
+    '''
+    new_results = []
+    for new_item in new_list:
+        id = new_item.get('id')
+        title = new_item.get('title')
+        name = new_item.get('name')
+        author = new_item.get('author')
+        description= new_description.get('description')
+        content = new_content.ge('content')
+
+        if poster:
+            new_object = New(id,name,author, description, content)
+            new_results.append(new_object)
+
+    return new_results
