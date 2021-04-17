0b7860ac68cd3e7f2da897791c4a2ac0b6998ec6
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-17 08:46:00 +0300

    Run.py initialized

--- app/views.py
+++ app/views.py
@@ -2,10 +2,21 @@ from flask import render_template
 from app import app
 
 # Views
+# Views
 @app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+
+    message = 'Into the badlands'
+    return render_template('index.html',message = message)
+
+@app.route('/new/<new_id>')
+def new(new_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('new.html',id = new_id)
