bddbd2f813cf18d8f1947f57710a2941c2d4e663
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-17 09:17:07 +0300

    Defining the values required

---
+++ app/models/new.py
@@ -0,0 +1,14 @@
+class New:
+    '''
+    New class to define New Objects
+    '''
+
+    def __init__(self,id,name,author, description, content):
+        self.id =id
+        self.name = name
+        self.title = title
+        self.author = author
+        self.description = description
+        self.content = content
+
+       
