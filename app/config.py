6cc16517c25109c8ab491933ec4bad137a7fa5a5
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-19 01:21:55 +0300

    Test News Sources

--- app/config.py
+++ app/config.py
@@ -1,11 +1,19 @@
+import os
+
 class Config:
     '''
     General configuration parent class
     '''
-    NEW_API_BASE_URL = 'https://newsapi.org/v2/everything?domains=wsj.com&apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v1/sources?country=us&apiKey={}'
 
     #https://api.themoviedb.org/3/movie/{}?api_key={}
     #https://newsapi.org/v2/everything?domains=wsj.com&apiKey={}
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    @staticmethod
+    def init_app(app):
+        pass
    
 
 class ProdConfig(Config):
