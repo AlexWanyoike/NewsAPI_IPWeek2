61cf3c262d38faf31572d11e790951e346df171a
AlexWanyoike <alex.wanyoike12@gmail.com>
2021-04-20 15:05:10 +0300

    Changes on the config

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,29 +1,18 @@
-from flask import Flask , Blueprint
+from flask import Blueprint
 main = Blueprint('main',__name__)
-from flask_bootstrap import Bootstrap
-from .config import DevConfig
-from . import views, error
+from . import views,error
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+def create_app(config_name):
+    app = Flask(__name__)
 
-  # Creating the app configurations
+    # Creating the app configurations
     app.config.from_object(config_options[config_name])
-    config_options[config_name].init_app(app)
+
     # Initializing flask extensions
     bootstrap.init_app(app)
 
-    # Will add the views and forms
-    
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting config
-    from .request import configure_request
-    configure_request(app)
-
     return app
-
-from app import views
-from app import error
